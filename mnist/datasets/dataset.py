cfcfc47b57d3eeb1a45c783e877f479ae39786e5
Kamilla Zyalaletdinova <krz142000@gmail.com>
2024-01-21 21:11:31 +0300

    hw2 prob

--- mnist/datasets/dataset.py
+++ mnist/datasets/dataset.py
@@ -1,6 +1,51 @@
-from PIL import Image
-from torch.utils.data import Dataset
+from typing import Optional
+
 import pandas as pd
+import pytorch_lightning as pl
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset, Subset
+from torchvision.transforms import transforms
+
+from mnist.utils.utils import get_data_dvc
+
+
+def get_datasets(
+    path, n1, n2, is_pred=False, mnist_dataset=True, has_labels=True, show_image=False
+):
+    get_data_dvc(path)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((n1,), (n2,))]
+    )
+    if mnist_dataset:
+        return MNISTDataset(path, transform=transform, is_pred=is_pred)
+    else:
+        return get_testdata(
+            path, transform=transform, has_labels=has_labels, show_image=show_image
+        )
+
+
+def get_testdata(path, transform=None, has_labels=True, show_image=False):
+    """
+    if has_labels==False: return empty list `labels`
+    """
+    csv_data = pd.read_csv(path)
+    labels = []
+    images = []
+    for i in range(csv_data.shape[0]):
+        image = Image.new("L", (28, 28))
+        if has_labels:
+            labels.append(csv_data.iloc[i, 0])
+            image.putdata(csv_data.iloc[i, 1:])
+        else:
+            print(f"Image №{i} opened in a new window")
+            image.putdata(csv_data.iloc[i, 0:])
+        if show_image:
+            image.show()
+        if transform:
+            images.append(transform(image))
+        else:
+            images.append(image)
+    return labels, images
 
 
 class MNISTDataset(Dataset):
@@ -31,3 +76,65 @@ class MNISTDataset(Dataset):
             if self.transform:
                 image = self.transform(image)
         return image, label
+
+
+class MNISTDataModule(pl.LightningDataModule):
+    def __init__(
+        self, batch_size, num_workers, val_size, path_train_val, path_test, n1, n2
+    ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.val_size = val_size
+        self.path_train_val = path_train_val
+        self.path_test = path_test
+        self.n1 = n1
+        self.n2 = n2
+
+    def setup(self, stage: Optional[str] = None):
+        self.train_val_dataset = get_datasets(self.path_train_val, self.n1, self.n2)
+        self.test_dataset = get_datasets(self.path_test, self.n1, self.n2)
+        self.pred_dataset = get_datasets(self.path_test, self.n1, self.n2, is_pred=True)
+        train_indexes = list(
+            range(0, int(len(self.train_val_dataset) * (1 - self.val_size)))
+        )
+        val_indexes = list(
+            range(
+                int(len(self.train_val_dataset) * (1 - self.val_size)),
+                len(self.train_val_dataset),
+            )
+        )
+        self.train_dataset = Subset(self.train_val_dataset, train_indexes)
+        self.val_dataset = Subset(self.train_val_dataset, val_indexes)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def predict_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.pred_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
---
+++ run_server.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+import git
+import hydra
+import mlflow
+import onnx
+import pandas as pd
+import torch
+from omegaconf import DictConfig
+
+from mnist.datasets.dataset import get_datasets
+from mnist.utils.utils import calculate_metrics, get_data_dvc
+
+
+@hydra.main(config_path="configs", config_name="config", version_base="1.3")
+def main(cfg: DictConfig) -> None:
+    model_path = cfg.model.model_path + ".onnx"
+    if not Path(model_path).exists():
+        print()
+        print("File .onnx will be downloaded from the DVC")
+        print("In other case: you need to run train.py and restart run_server.py")
+        get_data_dvc(model_path)
+        get_data_dvc(cfg.data.logs_path)
+    onnx_model = onnx.load(model_path)
+    input = torch.randn((cfg.model.in_channels, cfg.model.in_channels, 28, 28))
+    output = torch.randn((cfg.model.in_channels, cfg.model.out_channels))
+    mlflow.set_tracking_uri(cfg.artifacts.mlflow.tracking_uri)
+    with mlflow.start_run():
+        signature = mlflow.models.infer_signature(
+            input.detach().cpu().numpy(), output.detach().cpu().numpy()
+        )
+        model_info = mlflow.onnx.log_model(onnx_model, "model", signature=signature)
+        model_mlflow = mlflow.pyfunc.load_model(model_info.model_uri)
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
+        mlflow.set_tag("commit_id", sha)
+        labels, data_test = get_datasets(
+            cfg.data.test_data_path,
+            n1=cfg.preprocessing.n1,
+            n2=cfg.preprocessing.n2,
+            mnist_dataset=False,
+            has_labels=True,
+            show_image=False,
+        )
+        predictions_list = []
+        # inference model on test_datasets
+        for inp in data_test:
+            inp = torch.reshape(
+                inp, (cfg.model.in_channels, cfg.model.in_channels, 28, 28)
+            )
+            predictions_list.append(
+                model_mlflow.predict(inp.detach().cpu().numpy())["predictions"].argmax()
+            )
+        df_pred = pd.DataFrame(predictions_list, columns=["predicted_label"])
+        metrics = calculate_metrics(labels, df_pred["predicted_label"])
+        print("Metrics report")
+        print(metrics)
+        df_pred.to_csv(
+            cfg.data.pred_onnx_data_path, columns=["predicted_label"], index=False
+        )
+        print(f"Predictions saved to {cfg.data.pred_onnx_data_path}")
+
+        # new examples
+        labels, data_test = get_datasets(
+            cfg.data.exp_data_path,
+            n1=cfg.preprocessing.n1,
+            n2=cfg.preprocessing.n2,
+            mnist_dataset=False,
+            has_labels=True,
+            show_image=True,
+        )
+        for i, inp in enumerate(data_test):
+            print(f"Image №{i} opened in a new window")
+            print(f"True label for image № {i}:      {labels[i]}")
+            inp = torch.reshape(
+                inp, (cfg.model.in_channels, cfg.model.in_channels, 28, 28)
+            )
+            prediction = model_mlflow.predict(inp.detach().cpu().numpy())[
+                "predictions"
+            ].argmax()
+            print(f"Predicted label for image № {i}: {prediction}")
+
+
+if __name__ == "__main__":
+    main()
